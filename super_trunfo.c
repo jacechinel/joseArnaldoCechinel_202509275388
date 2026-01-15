af934a1f2ba95d4e3194378e0739ff094bb2e72f
jacechinel <65030663+jacechinel@users.noreply.github.com>
2026-01-15 23:18:43 +0000

    Criei o aplivativo super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+int main()
+{
+    //Declaracao das variáveis
+    char _estado1, _estado2;
+    char _codCarta1[5], _codCarta2[5], _cidade1[20], _cidade2[20];
+    int _populacao1, _pturisticos1, _populacao2, _pturisticos2;
+    float _area1, _pib1, _area2, _pib2;
+
+    //Recebendo os dados da Carta 1
+    printf("Digite os dados para a carta 1\n\n"); //Usando os dois \n aumenta o espaço e melhora a visualizacao
+    printf("Digite o Estado (use uma letra de A a H): ");
+    scanf(" %c", &_estado1);
+    printf("Digite o Codigo da Carta (Composto da letra do estado, mais o numero da cidade com 2 digitos): ");
+    scanf(" %s", _codCarta1);
+    printf("Digite o Nome da Cidade (Sem espacos, nao use nomes compostos): ");
+    scanf(" %s", _cidade1);
+    printf("Digite a Populacao desta Cidade: ");
+    scanf(" %d", &_populacao1);
+    printf("Digite a Area desta Cidade: ");
+    scanf(" %f", &_area1);
+    printf("Digite o PIB desta Cidade: ");
+    scanf(" %f", &_pib1);
+    printf("Digite o numero de Pontos Turisticos desta Cidade: ");
+    scanf(" %d", &_pturisticos1);
+
+    //Cria um espaco entre a aquisicao dos dados
+    // da Carta 1 para a Carta 2.
+    printf("\n\n");
+
+    //Recebendo os dados da Carta 2
+    printf("Digite os dados para a carta 2\n\n");  //Usando os dois \n aumenta o espaço e melhora a visualizacao
+    printf("Digite o Estado (use uma letra de A a H): ");
+    scanf(" %c", &_estado2);
+    printf("Digite o Codigo da Carta (Composto da letra do estado, mais o numero da cidade com 2 digitos): ");
+    scanf(" %s", _codCarta2);
+    printf("Digite o Nome da Cidade (Sem espacos, nao use nomes compostos): ");
+    scanf(" %s", _cidade2);
+    printf("Digite a Populacao desta Cidade: ");
+    scanf(" %d", &_populacao2);
+    printf("Digite a Area desta Cidade: ");
+    scanf(" %f", &_area2);
+    printf("Digite o PIB desta Cidade: ");
+    scanf(" %f", &_pib2);
+    printf("Digite o numero de Pontos Turisticos desta Cidade: ");
+    scanf(" %d", &_pturisticos2);
+
+    //Cria um espaco entre a aquisicao dos dados
+    // e a impressao deles.
+    printf("\n\n");
+
+    //Imprimindo as informacoes da carta 1
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", _estado1);
+    printf("Codigo: %s\n", _codCarta1);
+    printf("Nome da Cidade: %s\n", _cidade1);
+    printf("Populacao: %d\n", _populacao1);
+    printf("Area: %f Km\n", _area1);
+    printf("PIB: %f bilhoes de reais\n", _pib1);
+    printf("Numero de Pontos Turisticos: %d\n", _pturisticos1);
+
+    //Cria um espaco entre as informacoes
+    // das cidades.
+    printf("\n\n");
+
+    //Imprimindo as informacoes da carta 3
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", _estado2);
+    printf("Codigo: %s\n", _codCarta2);
+    printf("Nome da Cidade: %s\n", _cidade2);
+    printf("Populacao: %d\n", _populacao2);
+    printf("Area: %f Km\n", _area2);
+    printf("PIB: %f bilhoes de reais\n", _pib2);
+    printf("Numero de Pontos Turisticos: %d\n", _pturisticos2);
+
+    getchar();
+    getchar();
+
+    return 0;
+}
